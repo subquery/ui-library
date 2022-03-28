4a703723ae61729ddc939179858a05d71600db4d
greatguoyuhao <68301567+greatguoyuhao@users.noreply.github.com>
2022-03-28 16:43:32 +1300

    [release]

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-./node_modules/less/bin/lessc ./src/styles/overrides.less ./lib/overrides.css
+# ./node_modules/less/bin/lessc ./src/styles/overrides.less ./lib/overrides.css
 cp ./src/styles/overrides.less ./lib/overrides.less
 cp ./src/styles/variables.less ./lib/variables.less
