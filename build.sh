ed0a7e9c614ff2e56306c81bd8c491e0b23ef895
greatguoyuhao <68301567+greatguoyuhao@users.noreply.github.com>
2022-03-28 17:12:17 +1300

    [release] delete useless code about publish

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 # ./node_modules/less/bin/lessc ./src/styles/overrides.less ./lib/overrides.css
 cp ./src/styles/overrides.less ./lib/overrides.less
 cp ./src/styles/variables.less ./lib/variables.less
+cp ./src/styles/main.less ./lib/main.less
